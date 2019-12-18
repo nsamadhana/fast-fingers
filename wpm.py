0f3f7bb33dd750755c8ba8f1139c01b75271c2ea
Nevan Samadhana <nsamadhana@gmail.com>
2019-12-18 14:38:00 -0800

    Debugging action class functionality

--- wpm.py
+++ wpm.py
@@ -1,11 +1,16 @@
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 import time
+import sys 
+
+wpm = sys.argv[1]
+print(wpm)
 
 browser = webdriver.Firefox() #type is of webdriver 
 browser.get("https://10fastfingers.com/typing-test/english")
+action = Actions(driver); 
 
-print("retreiving parent")
+
 try: 
     row1 = browser.find_element_by_id("row1") #Grab div parent of span text 
     print("PARENT CONTAINER SUCCESFULLY RETRIEVED")
@@ -15,10 +20,23 @@ except:
 
 
 children = row1.find_elements_by_tag_name("span")
-print("Post child retrieval")
 
+print("number of items" , len(children))
 for item in children: #Stopping at last span element seen by the page 
     print(item.text) #type of item: str 
+    parseWord(self, item)
+
+
+browser.quit() #Closes firefox 
+
+
+def parseWord(self, word):
+    print("parseWord()")
+    for letter in word: 
+        action.sendKeys(letter)
+    action.sendKeys(Keys.SPACE) 
+
+
 
 
 
