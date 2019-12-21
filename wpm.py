c8324c0f5d8a8ee75d8f2f981e8432438fd1d6e0
Nevan Samadhana <nsamadhana@gmail.com>
2019-12-20 17:03:21 -0800

    actionClass sending letters/fix overflow

--- wpm.py
+++ wpm.py
@@ -10,35 +10,24 @@ browser.get("https://10fastfingers.com/typing-test/english")
 actions = ActionChains(browser)
 
 def main():
-    '''
-    try: 
-        row1 = browser.find_element_by_id("row1") #Grab div parent of span text 
-        print("PARENT CONTAINER SUCCESFULLY RETRIEVED")
-    except: 
-        print("Failed to retrieve parent parent container")
-        pass
-
-    children = row1.find_elements_by_tag_name("span")
-    '''
-    
     textContent = browser.find_element_by_id('wordlist').get_attribute('textContent')
     wordList = cleanUp(textContent)
     print(wordList)
     
+    for item in wordList: #Stopping at last span element seen by the page 
+        parseWord(item)
 
-    #for item in wordList: #Stopping at last span element seen by the page 
-     #   parseWord(item)
 
-
-    browser.quit() #Quit firefox 
+    browser.quit() 
     
 def parseWord(word):  
-    print("parseWord()")
+    assert type(word) is str
     for letter in str(word): 
-        actions.send_keys(word) #word
-        actions.send_keys(Keys.SPACE) #Spacebar 
+        actions.send_keys(letter).perform() #word
+        actions.send_keys(Keys.SPACE).perform() #Spacebar 
 
 def cleanUp(arr):
+    assert len(arr) > 1 
     temp = ""
     for item in arr:
         temp += item
