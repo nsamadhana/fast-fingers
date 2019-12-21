396b3f7e3c8bf352aaff82a121966340bb7ed807
Nevan Samadhana <nsamadhana@gmail.com>
2019-12-20 18:10:22 -0800

    Correctly inputs words | Loops first four words

--- wpm.py
+++ wpm.py
@@ -5,26 +5,32 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import sys 
 
+
 browser = webdriver.Firefox()
 browser.get("https://10fastfingers.com/typing-test/english")
 actions = ActionChains(browser)
+minute = 60 
+debug = open("debug.txt","w") 
 
 def main():
     textContent = browser.find_element_by_id('wordlist').get_attribute('textContent')
     wordList = cleanUp(textContent)
-    print(wordList)
-    
-    for item in wordList: #Stopping at last span element seen by the page 
+    for item in wordList:
+        print(item)
         parseWord(item)
-
+    
 
     browser.quit() 
     
 def parseWord(word):  
+    time.sleep(1.0) 
     assert type(word) is str
-    for letter in str(word): 
-        actions.send_keys(letter).perform() #word
-        actions.send_keys(Keys.SPACE).perform() #Spacebar 
+    for letter in word: 
+        print(letter)
+        actions.send_keys(letter) #word
+    actions.send_keys(Keys.SPACE).perform() #Spacebar
+    
+    
 
 def cleanUp(arr):
     assert len(arr) > 1 
