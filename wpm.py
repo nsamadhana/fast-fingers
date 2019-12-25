6e161e22bfdd159ad392e0d03cb14e9e1366b88a
Nevan Samadhana <nsamadhana@gmail.com>
2019-12-25 13:58:20 -0800

    Reading input correctly/Sending too quickly

--- wpm.py
+++ wpm.py
@@ -5,38 +5,32 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import sys 
 
-
 browser = webdriver.Firefox()
 browser.get("https://10fastfingers.com/typing-test/english")
 actions = ActionChains(browser)
-minute = 60 
+inputBox = browser.switch_to_active_element()
 debug = open("debug.txt","w") 
 
 def main():
     textContent = browser.find_element_by_id('wordlist').get_attribute('textContent')
     wordList = cleanUp(textContent)
-    for i in range(len(wordList)):
-        parseWord(wordList[i])
-    
-
-    browser.quit() 
+    print(wordList)
+    for word in wordList:
+        parseWord(word)
+ 
     
 def parseWord(word):  
-    time.sleep(1.0) 
     assert type(word) is str
     for letter in word: 
-        actions.send_keys(letter) #word
-    actions.send_keys(Keys.SPACE).perform() #Spacebar
-    
-    
-
+        inputBox.send_keys(letter)
+    inputBox.send_keys(Keys.SPACE)
+     
 def cleanUp(arr):
     assert len(arr) > 1 
     temp = ""
     for item in arr:
         temp += item
     return temp.split("|")
-    
 
 if __name__ == "__main__": 
     main()
