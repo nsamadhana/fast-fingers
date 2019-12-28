95a4204c91af854e60e026b1d75bc7920255a13f
Nevan Samadhana <nsamadhana@gmail.com>
2019-12-28 14:28:32 -0800

    Refactored/xpath working

--- wpm.py
+++ wpm.py
@@ -3,25 +3,38 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import sys 
+#To do:
+#Navigate through logging in etc    
+#Implement Recaptcha bypass 
+#un:fastfingersbot@gmail.com   pw:Squat5buckets! 
+#un:definitleynotabot pw:notabot1
 
-#Implement timer to break after 60 seconds 
-
+#0.1s delay ~ 327 wpm 
 browser = webdriver.Firefox()
 browser.get("https://10fastfingers.com/typing-test/english")
 inputBox = browser.switch_to_active_element()
-minute = 60
+email = "fastfingersbot@gmail.com"
+pw = "notabot1"
+logInXP = "/html/body/div[4]/div/div[4]/div/div[1]/div[5]/div[1]/div[1]/a"
+unXP = 
+pwXP = 
+
 
 def main():
-    wpm = sys.argv[1]
     textContent = browser.find_element_by_id('wordlist').get_attribute('textContent')
     wordList = cleanUp(textContent)
     t = time.time() + minute
-    #Stop sending keys after one minute 
-    while time.time() < t: 
+    #logging in 
+    logIn = browser.find_element_by_xpath()
+    logIn.click()
+    print("after log in ")
+    
+    '''
+    while time.time() < 60: 
         for word in wordList:
-            time.sleep(0.250)
+            time.sleep(0.1)
             parseWord(word)
-
+    '''
 def parseWord(word):  
     assert type(word) is str
     for letter in word: 
