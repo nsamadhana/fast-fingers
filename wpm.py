0bdce5d237b02727aa7b442ced596b6705eab3e3
Nevan Samadhana <nsamadhana@gmail.com>
2019-12-28 16:48:33 -0800

    Succesful login/keys sending/work on anticheat

--- wpm.py
+++ wpm.py
@@ -10,36 +10,44 @@ import sys
 #un:definitleynotabot pw:notabot1
 
 #0.1s delay ~ 327 wpm 
-browser = webdriver.Firefox()
-browser.get("https://10fastfingers.com/typing-test/english")
-inputBox = browser.switch_to_active_element()
+driver = webdriver.Firefox()
+driver.get("https://10fastfingers.com/typing-test/english")
 email = "fastfingersbot@gmail.com"
-pw = "notabot1"
-logInXP = "/html/body/div[4]/div/div[4]/div/div[1]/div[5]/div[1]/div[1]/a"
-unXP = 
-pwXP = 
+password = "notabot1"
+lPath = "/html/body/div[4]/div/div[4]/div/div[1]/div[5]/div[1]/div[1]/a"
+ePath = "//*[@id='UserEmail']"
+pPath = "//*[@id='UserPassword']"
 
 
 def main():
-    textContent = browser.find_element_by_id('wordlist').get_attribute('textContent')
+    logIn()
+    time.sleep(5)
+    textContent = driver.find_element_by_id('wordlist').get_attribute('textContent')
     wordList = cleanUp(textContent)
-    t = time.time() + minute
-    #logging in 
-    logIn = browser.find_element_by_xpath()
-    logIn.click()
-    print("after log in ")
-    
-    '''
-    while time.time() < 60: 
+    limit = time.time() + 60
+
+    while time.time() < limit: 
         for word in wordList:
-            time.sleep(0.1)
+            time.sleep(0.15)
             parseWord(word)
-    '''
+    
+
+def logIn():
+    temp = driver.find_element_by_xpath(lPath)
+    temp.click()
+    em = driver.find_element_by_xpath(ePath)
+    em.click()
+    em.send_keys(email)
+    pw = driver.find_element_by_xpath(pPath)
+    pw.click()
+    pw.send_keys(password)
+    pw.send_keys(Keys.ENTER)
+
 def parseWord(word):  
     assert type(word) is str
     for letter in word: 
-        inputBox.send_keys(letter)
-    inputBox.send_keys(Keys.SPACE)
+        driver.switch_to.active_element.send_keys(letter)
+    driver.switch_to.active_element.send_keys(Keys.SPACE)
      
 def cleanUp(arr):
     assert len(arr) > 1 
