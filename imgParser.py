01893cf76796b54599db107e10c8ab4b930bbf5a
Nevan Samadhana <nsamadhana@gmail.com>
2020-01-02 21:59:22 -0800

    image recognition script working

--- imgParser.py
+++ imgParser.py
@@ -2,9 +2,9 @@ try:
     from PIL import Image
 except ImportError:
     import Image
-import pytesseract
- 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\nevan\AppData\Local\Programs\Python\Python37-32\Lib\site-packages\pytesseract'
+import pytesseract as tess
+tess.pytesseract.tesseract_cmd = r'C:\Users\nevan\AppData\Local\Tesseract-OCR\tesseract.exe'
 
-# Simple image to string
-print(pytesseract.image_to_string(Image.open('img.png')))
+img = Image.open('img.png')
+text = tess.image_to_string(Image.open('img.png'))
+print(text)
